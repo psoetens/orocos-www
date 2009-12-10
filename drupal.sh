ffd2cf769b2f64856e1f63b5c4fbd371ad38d63b
Peter Soetens <peter@thesourceworks.com>
2009-12-10 22:19:32 +0100

    scripts: update server script with new login name.

--- drupal.sh
+++ drupal.sh
@@ -1,5 +1,5 @@
 echo "fetching changes from orocos.org:"
-rsync -Ciuavz --exclude="files/css" --exclude="stable" --exclude="devel" --exclude="rtt" --exclude="ocl" --exclude="kdl" --exclude="bfl" bruyninckxh@www.orocos.org:www.orocos.org/ drupal/ 
+rsync -Ciuavz --exclude="files/css" --exclude="stable" --exclude="devel" --exclude="rtt" --exclude="ocl" --exclude="kdl" --exclude="bfl" bruyninckxh2@www.orocos.org:www.orocos.org/ drupal/ 
 echo "sending changes to orocos.org:"
-rsync -Ciuavz drupal/ bruyninckxh@www.orocos.org:www.orocos.org/
+rsync -Ciuavz drupal/ bruyninckxh2@www.orocos.org:www.orocos.org/
 echo "Done."
