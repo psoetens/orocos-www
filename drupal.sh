9bab3b8c77725ed26618edfa660b0ab657b24178
Peter Soetens <peter@thesourceworks.com>
2011-03-24 17:03:08 +0100

    drupal.sh: don't use this script on the master branch, only on server branch.
    Signed-off-by: Peter Soetens <peter@thesourceworks.com>

--- drupal.sh
+++
@@ -1,5 +0,0 @@
-echo "fetching changes from orocos.org:"
-rsync -Ciuavz --exclude="files/css" --exclude="stable" --exclude="devel" --exclude="rtt" --exclude="ocl" --exclude="kdl" --exclude="bfl" bruyninckxh@www.orocos.org:www.orocos.org/ drupal/ 
-echo "sending changes to orocos.org:"
-rsync -Ciuavz drupal/ bruyninckxh@www.orocos.org:www.orocos.org/
-echo "Done."
